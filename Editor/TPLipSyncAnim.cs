f7a23f302d7bab2c30c50202ad295c8c258b95c1
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2025-01-16 02:24:55 +0900

    feat: add lipsynccreator

--- Editor/TPLipSyncAnim.cs
+++ Editor/TPLipSyncAnim.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,7 +10,7 @@ namespace moe.kyre.tool4tp
 {
     public static class TPLipSyncAnim
     {
-        private static void CreateClip(string dirPath, List<BlendShape> visemes)
+        private static void CreateClip(string rendererPath, string dirPath, List<BlendShape> visemes)
         {
             for (int i = 0; i < visemes.Count; i++)
             {
@@ -22,37 +23,53 @@ namespace moe.kyre.tool4tp
                     if (i == j) curve.AddKey(0f, 100f);
                     else curve.AddKey(0f, 0f);
 
-                    clip.SetCurve("", typeof(SkinnedMeshRenderer), visemes[j].name, curve);
+                    clip.SetCurve(rendererPath, typeof(SkinnedMeshRenderer), $"blendShape.{visemes[j].name}", curve);
                 }
 
                 AnimationClipSettings settings = AnimationUtility.GetAnimationClipSettings(clip);
                 settings.loopTime = false;
                 AnimationUtility.SetAnimationClipSettings(clip, settings);
-
-                AssetDatabase.CreateAsset(clip, $"{dirPath}/{visemes[i].name}.anim");
+                
+                AssetDatabase.CreateAsset(clip, Path.Combine(dirPath, $"{visemes[i].name}.anim").Replace("\\", "/"));
             }
 
             AssetDatabase.SaveAssets();
         }
-
-        public static void Create(string dirPath, List<BlendShape> visemes)
+        
+        public static string Create(string rendererPath, string dirPath, List<BlendShape> visemes)
         {
-            CreateClip(dirPath, visemes);
+            CreateClip(rendererPath, dirPath, visemes);
+
+            string createPath = Path.Combine(dirPath, "LipSync.controller").Replace("\\", "/");
             
-            AnimatorController ac = AnimatorController.CreateAnimatorControllerAtPath($"{dirPath}/LipSync.controller");
+            AnimatorController ac = AnimatorController.CreateAnimatorControllerAtPath(createPath);
             ac.AddParameter("Viseme", AnimatorControllerParameterType.Int);
             ac.AddParameter("Voice", AnimatorControllerParameterType.Float);
             
             AnimatorStateMachine stateMachine = ac.layers[0].stateMachine;
-            
-            string sil = visemes.First(v => v.name.Contains("sil")).name;
-            
+
             foreach (var viseme in visemes)
             {
-                AnimationClip visemeClip = AssetDatabase.LoadAssetAtPath<AnimationClip>($"{dirPath}/{viseme.name}.anim");
-                AnimatorState visemeState = stateMachine.AddState(viseme.name);
-                visemeState.motion = visemeClip;
+                AnimationClip clip = AssetDatabase.LoadAssetAtPath<AnimationClip>(Path.Combine(dirPath, $"{viseme.name}.anim").Replace("\\", "/"));
+                AnimatorState state = stateMachine.AddState(TPBlendShapes.Visemes.FirstOrDefault(v => viseme.name.Contains(v)));
+                
+                state.motion = clip;
+                
+                // Setting Motion Time to "Voice"
+                state.timeParameter = "Voice";
+                state.timeParameterActive = true;
+                state.writeDefaultValues = false;
+                
+                // 黄色いやつを設定
+                if (viseme.name.Contains("sil")) stateMachine.defaultState = state;
+
+                stateMachine.AddEntryTransition(state);
+                state.AddExitTransition();
             }
+
+            AssetDatabase.SaveAssets();
+
+            return createPath; 
         }
     }
 }
--- Editor/TPLipSyncWindow.cs
+++ Editor/TPLipSyncWindow.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.Animations;
 using nadena.dev.modular_avatar.core;
 using VRC.SDK3.Avatars.Components;
 
@@ -10,6 +11,7 @@ namespace moe.kyre.tool4tp
 {
     public class TPLipSyncWindow : EditorWindow
     {
+        private VRCAvatarDescriptor avatar = null;
         private SkinnedMeshRenderer local = null;
         public static List<BlendShape> blendShapes = new List<BlendShape>();
         
@@ -18,11 +20,20 @@ namespace moe.kyre.tool4tp
         {
             GetWindow<TPLipSyncWindow>("tool4tp/LipSync");
         }
+
+        private string GetRelativePath(Transform target, Transform root)
+        {
+            if (target == root)
+                return "";
+            return GetRelativePath(target.parent, root) + "/" + target.name;
+        }
         
         private void OnGUI()
         {
+            var avatarLocal = (VRCAvatarDescriptor)EditorGUILayout.ObjectField("Avatar", (VRCAvatarDescriptor)avatar, typeof(VRCAvatarDescriptor), true);
             var editorLocal = (SkinnedMeshRenderer)EditorGUILayout.ObjectField("舌ピアスなど", (SkinnedMeshRenderer)local, typeof(SkinnedMeshRenderer), true);
-
+            
+            if (avatarLocal != null && avatar == null) avatar = avatarLocal;
             if (editorLocal != null && local == null) local = editorLocal;
             
             if (local != null)
@@ -40,6 +51,19 @@ namespace moe.kyre.tool4tp
                     EditorGUILayout.LabelField(bs.name);
                     
                     EditorGUILayout.EndHorizontal();
+                }
+
+                if (GUILayout.Button("LipSync を生成する"))
+                {
+                    string fullPath = EditorUtility.SaveFolderPanel("フォルダを選択", "", "");
+                    string path = "Assets" + fullPath.Replace(Application.dataPath, "");
+                    
+                    if (!string.IsNullOrEmpty(path))
+                    {
+                        string controllerPath = TPLipSyncAnim.Create(GetRelativePath(editorLocal.transform, avatarLocal.transform), path, visemes);
+                        var animator = local.gameObject.AddComponent<Animator>();
+                        animator.runtimeAnimatorController = AssetDatabase.LoadAssetAtPath<RuntimeAnimatorController>(controllerPath);
+                    }
                 }
             }
         }
